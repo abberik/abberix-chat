71427383a36a458626179cb879b83a041c87a55a
abberik <albineriksson99@hotmail.com>
2016-06-01 23:01:59 +0200

    Update TestConfig.java

--- src/ver4/TestConfig.java
+++ src/ver4/TestConfig.java
@@ -1,7 +1,15 @@
 package ver4;
 
 public class TestConfig {
-
+	
+	
+	/*
+	*
+	* This file is for testing, its intendet to start a local config with two clients and a server,
+	* not neccessary for actually using the program but it is handy for testing
+	*
+	*/
+	
 	public static void main(String[] args) {
 		
 		Client a = new Client();
